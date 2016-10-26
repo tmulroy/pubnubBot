40afb547e85ea99fcd5031666ef8451d7969d531
tom <thomasjohnmulroy@gmail.com>
2016-10-26 14:40:45 -0400

    add listener

--- spike.js
+++ spike.js
@@ -21,8 +21,27 @@ function publishSampleMessage() {
   });
 }
 
-// pubnub.subscribe({
-//   channels: [process.env.CHANNEL],
-// });
+pubnub.addListener({
+  message: (m) => {
+    let channelName = m.channel;
+    let pubTT = m.timetoken;
+    let msg = m.message;
+  },
+  presence: function(p) {
+       // handle presence
+       let action = p.action; // Can be join, leave, state-change or timeout
+       let channelName = p.channel; // The channel for which the message belongs
+       let occupancy = p.occupancy; // No. of users connected with the channel
+       let publishTime = p.timestamp; // Publish timetoken
+       let timetoken = p.timetoken;  // Current timetoken
+   },
+   status: function(s) {
+     console.log(`status: ${s}`)
+   }
+});
+pubnub.subscribe({
+  channels: [process.env.CHANNEL],
+  withPresence: true
+});
 
 publishSampleMessage();
